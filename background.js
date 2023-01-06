9dab5a86c9ed06fdf317a4a40b6a9a964899253d
rgantzos <info@gantzos.com>
2023-01-06 06:28:06 -0800

    notifications

--- background.js
+++ background.js
@@ -11,6 +11,32 @@ const setMessageCount = async function () {
     const data = await response.json();
     chrome.action.setBadgeText({ text: data.count.toString() });
     chrome.action.setBadgeBackgroundColor({ color: "#9688F1" });
+    const lastMessageCount = (await chrome.storage.sync.get("message-count"))[
+      "message-count"
+    ];
+    await chrome.storage.sync.set({ "message-count": data.count });
+    if (lastMessageCount < data.count) {
+      const messages = await fetch("https://www.postlit.dev/my/messages/");
+      const messageData = await messages.json();
+      messageData.forEach(function (message, i) {
+        const messageTypes = {
+            "repost": `@${message.creator} just reposted your post!`,
+            "mention": `@${message.creator} just mentioned you in their post!`,
+            "comment-mention": `@${message.creator} just mentioned you in their comment!`,
+            "comment-reply": `@${message.creator} just replied to your comment!`,
+            "comment": `@${message.creator} just commented on your post!`,
+            "follow": `@${message.creator} just followed you!`,
+          }
+        if (i + 1 > lastMessageCount) {
+          chrome.notifications.create(message._id, {
+            title: "@" + message.creator + " on postLit",
+            message: messageTypes[message.type],
+            iconUrl: "/icons/icon128.png",
+            type: "basic",
+          });
+        }
+      });
+    }
   } else {
     chrome.action.setBadgeText({ text: "?" });
     chrome.action.setBadgeBackgroundColor({ color: "#f18888" });
